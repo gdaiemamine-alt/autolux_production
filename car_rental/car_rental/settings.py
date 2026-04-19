05da621576dc4c86dba0d4542aa8efb5576fc222
gdaiemamine-alt <gdaiemamine@gmail.com>
2026-04-19 20:48:53 +0100

    Ajout PostgreSQL

--- car_rental/car_rental/settings.py
+++ car_rental/car_rental/settings.py
@@ -5,6 +5,7 @@ Usage prod : export DJANGO_ENV=production
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,11 +83,13 @@ if IS_PRODUCTION and os.environ.get('DB_NAME'):
         }
     }
 else:
+     
+
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        'default': dj_database_url.config(
+            default='sqlite:///db.sqlite3',
+            conn_max_age=600
+        )   
     }
 
 # ── Validation mots de passe ───────────────────────────────────────────────────
